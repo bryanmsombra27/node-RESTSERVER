6acc9d2631e17013cb677c8b67564c0e0eca7de9
bryan <bryanjm96@gmail.com>
2020-05-03 01:53:08 -0500

    restServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,21 @@
 //PROCESS es un objeto global que siempre esta corriendo a lo largo de la aplicacion de node y tambien es actualizado dependiendo del environment donde esta corriendo
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+/////////////////////////////////////////
+///ENTORNO
+//////////////////////////////
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+////////////////////////////////////////
+//Base de DAtos
+////////////////////////////
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://shadow:yCrqVBBcwNy3oh9e@cluster0-vweeg.mongodb.net/cafes'
+}
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 require('./config/config')
 
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
+
 const bodyParser = require("body-parser");
 
 // create application/x-www-form-urlencoded parser
@@ -11,24 +13,19 @@ app.use(bodyParser.urlencoded({
 //parse application/json
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.json("Hello World");
-});
-app.post("/usuario", (req, res) => {
-  let body = req.body;
-  res.json({
-    persona: body
-  });
-});
-app.put("/usuario/:id", (req, res) => {
-  let id = req.params.id;
-  res.json({
-    id
-  });
-});
-app.delete("/usuario/:id", (req, res) => {
-  let id = req.params.id;
-  res.json("post World");
+app.use(require('./routes/usuario'));
+
+//Conectando con la base de datos:  el metodo connect puede recibir 3 parametros el primero es la ruta de la conexion hacia la base de datos(al estar de manera local es correcta esa ruta olo indicamos el puerto de conexion y el nombre de la bd a conectar)   
+//Segundo parametro un objeto necesario para evitar futuros problemas de conexion en nuevas versiones de node y como tercer parametro (opcional) una callback en caso de que la conexion sea negativa tira un error de conexion o caso contrario indica que se ha conectado correctamente
+mongoose.connect(process.env.URLDB, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+
+}, (err, res) => {
+  if (err) throw err;
+
+  console.log('Conexion exitosa con la BD')
 });
 
 app.listen(process.env.PORT, () => console.log(`Escuchando por el puerto ${process.env.PORT }`));
